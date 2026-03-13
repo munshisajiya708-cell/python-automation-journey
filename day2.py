300d270e33942e055cc08a5a195ee57e55f02c82
sajiya munshi <munshisajiya708@gmail.com>
2026-03-14 01:07:00 +0530

    Day 2: Variables, input programs

---
+++ day2.py
@@ -0,0 +1,36 @@
+#program 1
+name=input("Enter your name:  ")
+print("Hello:", name)
+
+#program 2
+a = int(input("Ente number:  "))
+b = int(input("Enter number:  "))
+print("Sum :",a+b)
+
+#program 3
+a = int(input("Enter number:  "))
+b = int(input("Enter number:   "))
+print("Substraction :",a-b)
+
+#program 4
+a = int(input("Enter number:  "))
+b = int(input("Enter number:   "))
+print("Square :",a*b)
+
+#program 5
+length =int(input("Enter number: "))
+width = int(input("Enter number:  "))
+print("Area:",length*width)
+
+# Mini project studennt information
+name = input("Enter your name:  ")
+age = int(input("Enter your age:  "))
+course=input("Enter your course:  ")
+city = input("enter your city: ")
+
+print(".....................Student Information.....................")
+print("Name:     ",name)
+print("age:      ",age)
+print("Course:   ",course)
+print("City:     ",city)
+
