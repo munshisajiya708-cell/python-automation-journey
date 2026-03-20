f0780d1e0a49186356816d585b0bee1f43984d8d
sajiya munshi <munshisajiya708@gmail.com>
2026-03-21 01:29:31 +0530

    Day 3: Data types practice programs

---
+++ day3_datatypes.py
@@ -0,0 +1,25 @@
+# Data types
+name="sajiya"
+age=38
+height=4.6
+is_student= True
+
+print("Name:",name)
+print(type(name))
+print("Age:",age)
+print(type(age))
+print("Height:",height)
+print(type(height))
+print("Student:",is_student)
+print(type(is_student))
+
+#Student information
+name="sajiya"
+age=38
+course="Python"
+print("*................Student information....................**")
+print("Name:",name)
+print("Age:",age)
+print("Course:",course)
+
+
